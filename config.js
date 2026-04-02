f1f747e5420a1093f58275f8a40cadf4f3c6be1c
Андрей Накимов <139959596+addito-5G@users.noreply.github.com>
2026-04-02 12:58:30 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.__APP_CONFIG__ = {
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  supabaseUrl: "https://jgjhvjdyehdbzwdlyrya.supabase.co",
+  supabaseAnonKey: "sb_publishable_Gt4RXgPDvwhao1ilu4NrPg_MtRJY_7s",
   appName: "iUnitRadar",
 };
